0c66c91f58d36a907c8a4d30d6ed68c4cfceed1d
taljbiny <taljbinin@gmail.com>
2026-06-07 02:51:56 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,15 +1,12 @@
-from telegram.ext import Application, CommandHandler, CallbackQueryHandler
+from telegram.ext import Updater, CommandHandler, CallbackQueryHandler
 from bot.config import BOT_TOKEN
-from bot.handlers_user import start, handle_buttons
-from bot.handlers_admin import admin_users, admin_wallet
+from bot.handlers import start, button_handler
 
-app = Application.builder().token(BOT_TOKEN).build()
+updater = Updater(BOT_TOKEN, use_context=True)
+dp = updater.dispatcher
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CallbackQueryHandler(handle_buttons))
+dp.add_handler(CommandHandler("start", start))
+dp.add_handler(CallbackQueryHandler(button_handler))
 
-# admin commands
-app.add_handler(CommandHandler("users", admin_users))
-app.add_handler(CommandHandler("wallet", admin_wallet))
-
-app.run_polling()
+updater.start_polling()
+updater.idle()
