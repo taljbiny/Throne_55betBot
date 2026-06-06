9b4a4212fc164e6e6805838dd501ffb51bf2e1f6
taljbiny <taljbinin@gmail.com>
2026-06-07 02:33:13 +0300

    Fix formatting and add admin command handlers

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,4 @@
 from telegram.ext import Application, CommandHandler, CallbackQueryHandler
-
 from bot.config import BOT_TOKEN
 from bot.handlers_user import start, handle_buttons
 from bot.handlers_admin import admin_users, admin_wallet
@@ -9,6 +8,7 @@ app = Application.builder().token(BOT_TOKEN).build()
 app.add_handler(CommandHandler("start", start))
 app.add_handler(CallbackQueryHandler(handle_buttons))
 
+# admin commands
 app.add_handler(CommandHandler("users", admin_users))
 app.add_handler(CommandHandler("wallet", admin_wallet))
 
