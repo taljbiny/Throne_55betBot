519c8e09cfb3c0bad1e0a1b30e64174cdefcdf27
taljbiny <taljbinin@gmail.com>
2026-06-07 02:06:24 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,12 @@
+BOT_TOKEN = "PUT_YOUR_BOT_TOKEN"
 
+DB_CONFIG = {
+    "host": "localhost",
+    "user": "root",
+    "password": "",
+    "database": "bot_db"
+}
+
+API_URL = "http://localhost:8000"
+
+ADMIN_IDS = [123456789]  # ضع Telegram ID الخاص فيك
