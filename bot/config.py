74630385c284688819851e2c20c146aa56e9be00
taljbiny <taljbinin@gmail.com>
2026-06-07 02:34:07 +0300

    Remove DB_CONFIG from bot/config.py
    Removed database configuration details from config.py.

--- bot/config.py
+++ bot/config.py
@@ -1,12 +1,5 @@
 BOT_TOKEN = "7871329256:AAHQmIQe_c3eURL3PlgnO6OqiM0p7gOh8gQ"
 
-DB_CONFIG = {
-    "host": "localhost",
-    "user": "root",
-    "password": "",
-    "database": "bot_db"
-}
-
 API_URL = "http://localhost:8000"
 
 ADMIN_IDS = [8219716285]  # ضع Telegram ID الخاص فيك
