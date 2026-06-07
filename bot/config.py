fd75c2b39308d840ac9e9f35b0d2b90e75434816
taljbiny <taljbinin@gmail.com>
2026-06-08 01:06:39 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -2,6 +2,10 @@ import os
 
 BOT_TOKEN = os.getenv("BOT_TOKEN")
 
+DATABASE_URL = os.getenv("DATABASE_URL")
+
 API_URL = "https://telegram-bot-qqqs.onrender.com"
 
-ADMIN_IDS = [8219716285]
+ADMIN_IDS = [
+    8219716285
+]
