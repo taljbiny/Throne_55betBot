15c41d2fc6a937e80ebd43def0d79011acb69ad9
taljbiny <taljbinin@gmail.com>
2026-06-07 18:24:25 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,5 +1,7 @@
-BOT_TOKEN = "7871329256:AAHQmIQe_c3eURL3PlgnO6OqiM0p7gOh8gQ"
+import os
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
 
 API_URL = "https://telegram-bot-qqqs.onrender.com"
 
-ADMIN_IDS = [8219716285]  # ضع Telegram ID الخاص فيك
+ADMIN_IDS = [8219716285]
