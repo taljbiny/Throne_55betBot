b96b7d821c915eb394d69f54fc5188fa86dbd028
taljbiny <taljbinin@gmail.com>
2026-06-08 01:26:58 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -3,6 +3,8 @@ print("BOT STARTING...")
 import threading
 
 from fastapi import FastAPI
+import uvicorn
+
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -13,11 +15,10 @@ from bot.config import BOT_TOKEN
 from bot.db import init_db
 from bot.handlers_user import start, button_handler
 
-
 # إنشاء الجداول
 init_db()
 
-# إنشاء تطبيق التلغرام
+# تطبيق التلغرام
 telegram_app = Application.builder().token(BOT_TOKEN).build()
 
 telegram_app.add_handler(
@@ -25,4 +26,39 @@ telegram_app.add_handler(
 )
 
 telegram_app.add_handler(
-    CallbackQueryHandler(button
+    CallbackQueryHandler(button_handler)
+)
+
+
+def run_bot():
+    print("BOT IS RUNNING...")
+    telegram_app.run_polling(
+        drop_pending_updates=True
+    )
+
+
+# تشغيل البوت في Thread منفصل
+threading.Thread(
+    target=run_bot,
+    daemon=True
+).start()
+
+
+# FastAPI لإبقاء Render Web Service شغال
+app = FastAPI()
+
+
+@app.get("/")
+async def root():
+    return {
+        "status": "online",
+        "service": "telegram bot"
+    }
+
+
+if __name__ == "__main__":
+    uvicorn.run(
+        app,
+        host="0.0.0.0",
+        port=10000
+    )
