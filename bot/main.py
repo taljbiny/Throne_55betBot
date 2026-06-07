20f5d3e45b2d9bc1e42dfb5af93dce53127c44fb
taljbiny <taljbinin@gmail.com>
2026-06-08 00:15:01 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,5 +1,6 @@
 print("BOT STARTING...")
 
+import asyncio
 from telegram.ext import (
     Application,
     CommandHandler,
@@ -9,9 +10,20 @@ from telegram.ext import (
 from bot.config import BOT_TOKEN
 from bot.handlers_user import start, button_handler
 
-app = Application.builder().token(BOT_TOKEN).build()
 
-app.add_handler(CommandHandler("start", start))
-app.add_handler(CallbackQueryHandler(button_handler))
+async def main():
+    app = Application.builder().token(BOT_TOKEN).build()
 
-app.run_polling()
+    app.add_handler(CommandHandler("start", start))
+    app.add_handler(CallbackQueryHandler(button_handler))
+
+    await app.initialize()
+    await app.start()
+    await app.updater.start_polling()
+
+    while True:
+        await asyncio.sleep(3600)
+
+
+if __name__ == "__main__":
+    asyncio.run(main())
