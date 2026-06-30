a8d6a2b0bc592cd787ec103c220273298a132ccb
taljbiny <taljbinin@gmail.com>
2026-06-30 19:51:47 +0300

    Update config.py

--- bot/config.py
+++ bot/config.py
@@ -1,1 +1,11 @@
+from dotenv import load_dotenv
+import os
 
+load_dotenv()
+
+BOT_TOKEN = os.getenv("BOT_TOKEN")
+
+USERNAME = os.getenv("USERNAME")
+PASSWORD = os.getenv("PASSWORD")
+
+BASE_URL = "https://agents.55bets.net/global/api"
