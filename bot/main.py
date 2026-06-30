a02429180a8d2f20ea16c842327d7e5e3e5cda51
taljbiny <taljbinin@gmail.com>
2026-06-30 20:05:21 +0300

    Update main.py

--- bot/main.py
+++ bot/main.py
@@ -1,1 +1,17 @@
 
+from telegram.ext import (
+    Application,
+    CommandHandler,
+    CallbackQueryHandler,
+)
+
+from bot.config import BOT_TOKEN
+from bot.handlers import start, button_handler
+
+
+app = Application.builder().token(BOT_TOKEN).build()
+
+app.add_handler(CommandHandler("start", start))
+app.add_handler(CallbackQueryHandler(button_handler))
+
+app.run_polling()
