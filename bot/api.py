7d723ccde0a32932094fa01602437f81aaf42827
taljbiny <taljbinin@gmail.com>
2026-06-30 19:52:03 +0300

    Update api.py

--- bot/api.py
+++ bot/api.py
@@ -1,1 +1,25 @@
+import requests
 
+from bot.config import BASE_URL
+from bot.config import USERNAME
+from bot.config import PASSWORD
+
+
+session = requests.Session()
+
+
+def login():
+
+    payload = {
+        "username": USERNAME,
+        "password": PASSWORD
+    }
+
+    r = session.post(
+        f"{BASE_URL}/User/signIn",
+        json=payload
+    )
+
+    data = r.json()
+
+    return data["status"]
