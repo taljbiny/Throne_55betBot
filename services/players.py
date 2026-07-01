b0757a01827f2f5be987ccb1a9889d9437a8b5a2
taljbiny <taljbinin@gmail.com>
2026-07-01 23:49:38 +0300

    Update players.py

--- services/players.py
+++ services/players.py
@@ -1,1 +1,20 @@
+from services.api import post
 
+
+def get_players(start=0, limit=10):
+
+    payload = {
+        "start": start,
+        "limit": limit,
+        "filter": {}
+    }
+
+    response = post(
+        "Statistics/getPlayersStatisticsPro",
+        payload
+    )
+
+    try:
+        return response.json()
+    except Exception:
+        return None
