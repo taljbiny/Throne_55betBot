66e29d74acfdd0303e7cbfab8cb4c77b3ba8ee33
taljbiny <taljbinin@gmail.com>
2026-07-01 15:45:43 +0300

    Update auth.py

--- services/auth.py
+++ services/auth.py
@@ -1,1 +1,22 @@
+from bot.config import USERNAME, PASSWORD
+from services.api import post
 
+
+def login():
+
+    payload = {
+        "username": USERNAME,
+        "password": PASSWORD
+    }
+
+    response = post(
+        "User/signIn",
+        payload
+    )
+
+    try:
+        data = response.json()
+    except Exception:
+        return False
+
+    return data.get("status", False)
